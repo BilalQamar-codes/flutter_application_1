25ab1e2baf0a273d6f3cbc9080b4810d590499d9
QuantamCoder3941 <108925559+QuantamCoder3941@users.noreply.github.com>
2023-01-25 16:46:31 +0500

    animation

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -1,13 +1,21 @@
-// ignore_for_file: prefer_const_constructors, duplicate_ignore, camel_case_types, file_names, avoid_print
+// ignore_for_file: prefer_const_constructors, duplicate_ignore, camel_case_types, file_names, avoid_print, unused_import, unnecessary_import, sort_child_properties_last, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_application_1/utills/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Login_Page extends StatelessWidget {
+class Login_Page extends StatefulWidget {
   const Login_Page({super.key});
 
+  @override
+  State<Login_Page> createState() => _Login_PageState();
+}
+
+class _Login_PageState extends State<Login_Page> {
+  String name = "";
+  String password = "";
+  bool changebutton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -26,7 +34,7 @@ class Login_Page extends StatelessWidget {
               height: 20.0,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900),
             ),
             SizedBox(
@@ -38,11 +46,21 @@ class Login_Page extends StatelessWidget {
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: ((value) {
+                      name = value;
+                      setState(() {});
+                    }),
                     decoration: InputDecoration(
-                        hintText: "Enter User Name", labelText: "UserName"),
+                      hintText: "Enter User Name",
+                      labelText: "UserName",
+                    ),
                   ),
                   TextFormField(
                     obscureText: true,
+                    onChanged: ((value) {
+                      password = value;
+                      setState(() {});
+                    }),
                     decoration: InputDecoration(
                         hintText: "Enter Password", labelText: "Password"),
                   )
@@ -52,13 +70,44 @@ class Login_Page extends StatelessWidget {
             SizedBox(
               height: 20.0,
             ),
-            ElevatedButton(
-              style: TextButton.styleFrom(minimumSize: Size(100, 35)),
-              onPressed: () {
+            // ElevatedButton(
+            //   style: TextButton.styleFrom(minimumSize: Size(100, 35)),
+            //   onPressed: () {
+            //
+            //   },
+            //   child: Text("Login"),
+            // ),
+            InkWell(
+              onTap: () async {
+                setState(() {
+                  changebutton = true;
+                });
+                await Future.delayed(Duration(seconds: 1));
                 Navigator.pushNamed(context, MyRoutes.homeroute);
               },
-              child: Text("Login"),
-            ),
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                height: 50,
+                width: changebutton ? 50 : 100,
+                alignment: Alignment.center,
+                child: changebutton
+                    ? Icon(
+                        Icons.done,
+                        color: Colors.white,
+                      )
+                    : Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(changebutton ? 22 : 8),
+                ),
+              ),
+            )
           ],
         ),
       ),
